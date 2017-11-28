f42da2cddccf9dff823dad6d55ff77ad04653777
Vaguehorizon <Vaguehorizon@users.noreply.github.com>
2017-11-28 10:19:39 +0000

    Inject values in MainActivity
    Previous dependency removed.
    
    Values successfully received.

--- app/src/main/java/com/muelpatmore/daggerdemo/MainActivity.java
+++ app/src/main/java/com/muelpatmore/daggerdemo/MainActivity.java
@@ -2,15 +2,30 @@ package com.muelpatmore.daggerdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.muelpatmore.daggerdemo.injection.TrainingModule;
+
+import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Inject
+    TrainingScores trainingScores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        injectDagger();
 
-        TrainingScores trainingScores = new TrainingScores();
+//        TrainingScores trainingScores = new TrainingScores();
         String a= trainingScores.mScore;
+
+        Toast.makeText(this, a, Toast.LENGTH_SHORT).show();
+    }
+
+    public void injectDagger(){
+        ((MyApp)getApplicationContext()).getTrainingComponent().inject(this);
     }
 }
